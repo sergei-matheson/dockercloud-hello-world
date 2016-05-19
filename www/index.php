723adc84de3828fa4bc30f375e81b97e3a135efb
Sergei Matheson <sergei.matheson@gmail.com>
2016-05-19 10:34:58 +1000

    Hi

--- www/index.php
+++ www/index.php
@@ -17,6 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
+Hi cloud-sig
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
